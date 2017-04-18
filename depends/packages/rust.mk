d1e7dc343e7d2e8ef3a66c68374fa32990a32082
ca333 <ca333@safe-mail.net>
2017-04-18 15:53:43 +0200

    use mingw windows target

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -1,8 +1,8 @@
 package=rust
 $(package)_version=1.16.0
 $(package)_download_path=https://static.rust-lang.org/dist
-$(package)_file_name=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash=48621912c242753ba37cad5145df375eeba41c81079df46f93ffb4896542e8fd
+$(package)_file_name=rust-$($(package)_version)-i686-pc-windows-gnu.tar.gz
+$(package)_sha256_hash=5e1ccb48e96a7f5299bf5c5af9bc2c5f569b3c483b9a44f911bf989ae270e163
 
 define $(package)_stage_cmds
   ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native --disable-ldconfig
